7698786e28f506d3ef64f77ba02a2c70d7ab669e
Duarte <Duarte.amarelo@hotmail.com>
2020-06-17 23:01:30 +0200

    Add Basic Spring and React Projects

---
+++ hose_abe_jass_backend/src/main/java/ch/zli/hose_abe_jass_backend/HoseAbeJassBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.zli.hose_abe_jass_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoseAbeJassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoseAbeJassBackendApplication.class, args);
+	}
+
+}
---
+++ hose_abe_jass_backend/src/test/java/ch/zli/hose_abe_jass_backend/HoseAbeJassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zli.hose_abe_jass_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoseAbeJassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
