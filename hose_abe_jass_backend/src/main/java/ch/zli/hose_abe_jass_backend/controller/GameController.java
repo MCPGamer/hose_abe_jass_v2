0b2caf027537dd4a4234da95b9f042cf63c0e2eb
godu <duarte.goncalves@aeis.ch>
2020-06-18 15:37:37 +0200

    Add Backend for Joining Room

--- hose_abe_jass_backend/src/main/java/ch/zli/hose_abe_jass_backend/controller/GameController.java
+++ hose_abe_jass_backend/src/main/java/ch/zli/hose_abe_jass_backend/controller/GameController.java
@@ -2,8 +2,11 @@ package ch.zli.hose_abe_jass_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.zli.hose_abe_jass_backend.exception.JoinRoomException;
 import ch.zli.hose_abe_jass_backend.model.RoomService;
 
 @CrossOrigin(origins="*")
@@ -13,4 +16,13 @@ public class GameController {
 	private RoomService roomService;
 	
 	//TODO: rest functions for this
+	
+	@PostMapping("/joinRoom")
+	public Object joinRoom(@RequestBody String name, @RequestBody String roomCode) {
+		try {
+			return roomService.joinRoom(name, roomCode);
+		} catch (JoinRoomException ex) {
+			return ex.getErrorMessage();
+		}
+	}
 }
--- hose_abe_jass_backend/src/main/java/ch/zli/hose_abe_jass_backend/model/RoomService.java
+++ hose_abe_jass_backend/src/main/java/ch/zli/hose_abe_jass_backend/model/RoomService.java
@@ -6,11 +6,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 
+import ch.zli.hose_abe_jass_backend.exception.JoinRoomException;
+
 @Service
 @ApplicationScope
 public class RoomService {
 	@Autowired
 	private ArrayList<GameHandler> gameHandlers = new ArrayList<>();
-	
+
 	//TODO: All Methods for Creating / Joining rooms go here
+
+	public Room joinRoom(String name, String roomCode) throws JoinRoomException{
+		Room room= null;
+		boolean playerAdded = false;
+		boolean nameDuplicate = false;
+		
+		for(GameHandler handler : gameHandlers) {
+			if(roomCode.toUpperCase().equals(handler.getRoom().getRoomCode())) {
+				room = handler.getRoom();
+			}
+		}
+		
+		if(room == null) {
+			throw new JoinRoomException("No Room exists with that Code");
+		}
+		
+		Player[] players = room.getPlayers();
+		for(int i = 0; i < 12; i++) {
+			if(players[i] == null) {
+				players[i] = new Player(name);
+				playerAdded = true;
+				break;
+			} else {
+				if(players[i].getName().equals(name)) {
+					nameDuplicate = true;
+					break;
+				}
+			}
+		}
+		
+		if(!playerAdded) {
+			throw new JoinRoomException("Room is already full");
+		}
+		
+		if(nameDuplicate) {
+			throw new JoinRoomException("A Player in that Room already has that Name");
+		}
+		
+		return room;
+	}
 }
