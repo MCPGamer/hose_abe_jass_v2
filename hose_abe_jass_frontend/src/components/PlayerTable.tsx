fe209e66735975e16923993915029064bc83ea2a
godu <duarte.goncalves@aeis.ch>
2020-06-22 09:04:48 +0200

    Add PlayerTable to the GameScreen

---
+++ hose_abe_jass_frontend/src/components/PlayerTable.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './PlayerTable.css';
+import {Room} from '../models/room';
+
+type Props = {
+  room: Room;
+  player: string;
+  revealCards?: boolean;
+}
+
+export const PlayerTable: React.FC<Props> = (props) => {
+
+  return (
+      <div className={'player-container'}>
+        <table>
+          <tbody>
+          {props.room.players.map((p) => (
+            p !== null ?
+              <tr key={p.name}>
+                <td className={props.room.players[props.room.playerturn].name == p.name ? 'bold' : ''}>
+                  {p.name === props.player ? p.name + '(you)' : p.name}
+                  {props.revealCards ? p.cards.map((c) => (
+                    <image className={'small-card'} href={c.cardColor + c.cardValue + 'png'}/>
+                  )): ''}
+                </td>
+              </tr>
+            : ''
+            ))}
+          </tbody>
+        </table>
+      </div>
+  );
+};
--- hose_abe_jass_frontend/src/containers/GameScreen.tsx
+++ hose_abe_jass_frontend/src/containers/GameScreen.tsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import {RoomManager} from '../components/RoomManager';
-import './MainMenu.css'
+import './GameScreen.css';
 import {Room} from '../models/room';
 import {RoomCode} from '../components/RoomCode';
+import {PlayerTable} from '../components/PlayerTable';
 
 type Props = {
   room: Room,
@@ -10,8 +10,11 @@ type Props = {
 };
 
 export const GameScreenContainer: React.FC<Props> = (props) => {
+    const [enabled, setEnabled] = useState<boolean>(props.room.players[props.room.playerturn].name === props.player);
+
     return (
         <div className="center-form">
+          <PlayerTable room={props.room} player={props.player}/>
           you're in the game
           <RoomCode code={props.room.roomCode}/>
         </div>
