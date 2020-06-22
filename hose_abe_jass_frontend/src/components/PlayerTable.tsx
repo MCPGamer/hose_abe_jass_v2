994574478f7fcf89ea40f28bcb37de10d5b4ec88
godu <duarte.goncalves@aeis.ch>
2020-06-22 11:19:14 +0200

    Add Cardimages

--- hose_abe_jass_frontend/src/components/PlayerTable.tsx
+++ hose_abe_jass_frontend/src/components/PlayerTable.tsx
@@ -9,23 +9,23 @@ type Props = {
 }
 
 export const PlayerTable: React.FC<Props> = (props) => {
-
   return (
       <div className={'player-container'}>
         <table>
           <tbody>
-          {props.room.players.map((p) => (
-            p !== null ?
-              <tr key={p.name}>
-                <td className={props.room.players[props.room.playerturn].name == p.name ? 'bold' : ''}>
-                  {p.name === props.player ? p.name + '(you)' : p.name}
-                  {props.revealCards ? p.cards.map((c) => (
-                    <image className={'small-card'} href={c.cardColor + c.cardValue + 'png'}/>
-                  )): ''}
-                </td>
-              </tr>
-            : ''
-            ))}
+          {props.room.players.map((p)=>(
+              p!==null?
+                  <tr key={p.name}>
+                    <td className={props.room.players[props.room.playerturn].name == p.name ? 'bold' : ''}>
+                      {p.name === props.player ? p.name + '(you)' : p.name}
+                      {props.revealCards ? p.cards.map((c) => (
+                          <image className={'small-card'}
+                                 href={'img/jasskarten/' + c.cardColor + c.cardValue + 'Klein.gif'}/>
+                                 )) : ''}
+                    </td>
+                  </tr>
+                  : ''
+          ))}
           </tbody>
         </table>
       </div>
