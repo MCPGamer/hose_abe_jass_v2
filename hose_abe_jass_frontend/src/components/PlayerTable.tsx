d188d85784dee33e0b66d7db1cf42793af76e66c
godu <duarte.goncalves@aeis.ch>
2020-06-22 15:02:26 +0200

    Optimize Validation for safety

--- hose_abe_jass_frontend/src/components/PlayerTable.tsx
+++ hose_abe_jass_frontend/src/components/PlayerTable.tsx
@@ -16,7 +16,7 @@ export const PlayerTable: React.FC<Props> = (props) => {
           {props.room.players.map((p)=>(
               p!==null?
                   <tr key={p.name}>
-                    <td className={props.room.players[props.room.playerturn].name == p.name ? 'bold' : ''}>
+                    <td className={props.room.players[props.room.playerturn].name === p.name ? 'bold' : ''}>
                       {p.name === props.player ? p.name + '(you)' : p.name}
                       {props.revealCards ? p.cards.map((c) => (
                           <image className={'small-card'}
